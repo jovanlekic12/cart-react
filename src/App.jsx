0efc355b4b6101fb9e28340ee50fe2944f98bf76
jovanlekic12 <lekicjovan1212@gmail.com>
2024-09-27 23:23:49 +0200

    add fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
-import './App.css'
-
+import { useState } from "react";
+import "./App.css";
+const url = "https://course-api.com/react-useReducer-cart-project";
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    </>
-  )
+  const [count, setCount] = useState(0);
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setTours(data);
+    } catch (error) {
+      setError(error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return <></>;
 }
 
-export default App
+export default App;
