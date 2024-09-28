5f7710b37f01f8e4f2ab72721f0c598b98ae09fb
jovanlekic12 <lekicjovan1212@gmail.com>
2024-09-29 00:56:42 +0200

    refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,66 @@ import "./App.css";
 const url = "https://fakestoreapi.com/products";
 function App() {
   const [phones, setPhones] = useState([]);
+  const [total, setTotal] = useState(0);
   const [error, setError] = useState(null);
+  const [amount, setAmount] = useState(1);
+
+  function handleDeletePhone(id) {
+    setPhones((prev) => prev.filter((phone) => phone.id !== id));
+  }
+
+  function increaseAmount() {
+    if (amount > 0) return setAmount(amount + 1);
+  }
+
+  function calculateTotalPrice() {
+    let totalPrice = 0;
+    for (let i = 0; i < phones.length; i++) {
+      totalPrice += phones[i].price;
+    }
+    return Math.trunc(totalPrice);
+  }
   const fetchData = async () => {
     try {
       const response = await fetch(url);
       const data = await response.json();
       console.log(data);
+      //const phones with amount
+      //
       setPhones(data);
     } catch (error) {
       setError(error);
     }
   };
+
   useEffect(() => {
     fetchData();
   }, []);
+
+  useEffect(() => {
+    console.log("pozivam se");
+    setTotal(calculateTotalPrice);
+  }, [phones]);
   return (
     <main className="main__container">
       {error && <p>Error: {error.message}</p>}
       <h1>YOUR BAG</h1>
       <ul className="list">
         {phones.map((phone) => {
-          return <Phone key={phone.id} {...phone} />;
+          return (
+            <Phone
+              key={phone.id}
+              {...phone}
+              handleDeletePhone={handleDeletePhone}
+              amount={amount}
+              increaseAmount={increaseAmount}
+            />
+          );
         })}
+        <div className="total__container">
+          <h3 className="total">Total:</h3>
+          <h3 className="total__price">{total}$</h3>
+        </div>
       </ul>
     </main>
   );
--- src/phone.jsx
+++ src/phone.jsx
@@ -2,18 +2,21 @@ import React from "react";
 import { useState } from "react";
 import { FaAngleUp, FaAngleDown } from "react-icons/fa";
 function Phone(props) {
-  const { id, title, price, image } = props;
-  const [amount, setAmount] = useState(1);
+  const { id, title, price, image, handleDeletePhone, amount, increaseAmount } =
+    props;
+
   return (
     <li className="list__item">
       <img src={image} alt="404" className="image" />
       <div className="title__container">
         <h6 className="title">{title}</h6>
         <p className="price">{price}$</p>
-        <button className="btn">remove</button>
+        <button onClick={() => handleDeletePhone(id)} className="btn">
+          remove
+        </button>
       </div>
       <div className="amount__container">
-        <FaAngleUp />
+        <FaAngleUp onClick={increaseAmount} />
         <h5 className="amount">{amount}</h5>
         <FaAngleDown />
       </div>
